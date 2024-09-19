ebb5dbfb08bec5d7cdd456ec16964e60880c1c91
PopeyedBird6 <isaac45604@gmail.com>
2024-09-18 23:38:06 -0600

    18/09/2024 Marcos Montes Isaac Alejandro

--- js.js
+++ js.js
@@ -9,6 +9,14 @@ const nombre = "Iaac";
 let edad = 17;
 var edad2 = 12321;
 
+
+let a = 5;
+let b = 2;
+
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let divicion = a/b;
 /*
 Tipos de datos:
 string, number, float, bigInt, boolean, char, NaN, Infinity, hexadecimal, objects 
