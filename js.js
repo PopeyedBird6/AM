232cb07b7dca3ed4775f1d18a4959a7d9bcb8fef
PopeyedBird6 <isaac45604@gmail.com>
2024-09-18 20:51:53 -0600

    primera clase

---
+++ js.js
@@ -0,0 +1,15 @@
+//Comentario en una sola linea
+
+/*
+Comentario en varias lineas
+*/
+console.log("hola mundo");
+alert("Holi");
+const nombre = "Iaac";
+let edad = 17;
+var edad2 = 12321;
+
+/*
+Tipos de datos:
+string, number, float, bigInt, boolean, char, NaN, Infinity, hexadecimal, objects 
+*/
