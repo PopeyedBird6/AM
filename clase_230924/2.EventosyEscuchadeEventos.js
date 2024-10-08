2d516cbdcc86353a1dd3efbc03f3edafa2507974
PopeyedBird6 <isaac45604@gmail.com>
2024-10-07 19:18:54 -0600

    o

--- clase_230924/2.EventosyEscuchadeEventos.js
+++ clase_230924/2.EventosyEscuchadeEventos.js
@@ -1,8 +1,10 @@
 // Obtener el botón del DOM
-const botonColor = document.getElementById('cambiarColor');
+const botonColor = document.getElementById('cambiarcolor');
 
 //Añadir un listener al botón para manejar el evento click
-botonColor.addEventListener('click', () => {
+botonColor.addEventListener("click", () => {
     //Generar un color aleatorio
     const colorAleatorio = `#${Math.floor(Math.random()*1677215).toString(16)}`;
+    //Cambiar el color de fondo del body
+    document.body.style.backgroundColor = colorAleatorio;
 });
