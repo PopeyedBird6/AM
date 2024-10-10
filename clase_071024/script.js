1f6903e898fb8c640e39fdf261a49f6099c5583b
PopeyedBird6 <isaac45604@gmail.com>
2024-10-09 20:40:20 -0600

    Marcos Montes Isaac Alejandro 5IV9

--- clase_071024/script.js
+++ clase_071024/script.js
@@ -1,18 +1,22 @@
+//Agrega el valor a la pantalla de la calculadora 
 function appendToDisplay(value){
     document.getElementById('display').value += value;
 }
+//Borra cualquier dato de la pantalla de la calculadora
 function clearDisplay(){
     document.getElementById('display').value = '';
 }
+//Borra el ultimo valor agregado en la pantalla de la calculadora
 function deleteLast(){
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
+//Calcula la operación dada.
 function calculateResult(){
     try{
         let result = eval(document.getElementById('display').value);
         document.getElementById('display').value = result;
-    } catch (error){
+    } catch (error){                    //por si el usuario mete un valor que no vale.
         alert('Expresión inválida');
     }
 }
