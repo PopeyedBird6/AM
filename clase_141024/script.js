86a8d53eeed966e60036eeff10b2fd8bbcf8b604
PopeyedBird6 <isaac45604@gmail.com>
2024-10-14 20:13:23 -0600

    MARCOS MONTES ISAAC ALEJANDRO 5IV9

---
+++ clase_141024/script.js
@@ -0,0 +1,55 @@
+//FUNCIONES
+/*function hello() {
+    console.log('Hola Js');    
+}
+hello();
+
+function hello(){
+    return 'Hola Js'
+}
+
+const result = hello();
+console.log(result);
+
+hello();
+
+//retorna objeto vacio
+function hello() {
+    return{};    
+}
+console.log(hello());
+
+function hello() {
+    return{
+        nombre: 'Raul'
+    }    
+}
+
+console.log(hello());
+
+function hello() {
+    return function (){
+        return 'Hola Js'
+    }    
+}
+
+console.log(hello()());
+
+//uso de parametros de funciones
+function helloconJS(name){
+    return 'Hola ' + name
+}
+console.log('Antonio');
+
+function helloconJS(x,y) {
+    return x + y;    
+}
+
+console.log(helloconJS(10,20));
+
+//control de errores de parametros
+function hello(x,y=0) {
+    return x + y;    
+}
+console.log(hello(10));
+*/
