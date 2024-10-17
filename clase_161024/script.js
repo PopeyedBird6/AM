c50f55bfb583e03f90b7ce7c3e185c5a710533d3
PopeyedBird6 <isaac45604@gmail.com>
2024-10-16 20:41:22 -0600

    MARCOS MONTES ISAAC ALEJANDRO 5IV9

---
+++ clase_161024/script.js
@@ -0,0 +1,8 @@
+const documento = document.createElement('h1');
+
+documento.textContent= 'hola clase de DOM desde js jajajaxd';
+
+
+document.querySelector('body').appendChild(documento);
+/* console.log(documento.textContent); */
+document.body.appendChild(documento);
